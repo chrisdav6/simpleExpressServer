2b86619f379107b70a0c71c2297046a4434a4467
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-05-21 13:48:28 -0400

    testing nginx with different port numbers

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const port = process.env.port || 3000;
 const app = express();
 
 app.set("view engine", "ejs");
@@ -7,6 +8,6 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log("Server started");
 });
